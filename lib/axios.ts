5ee6a70d19ee9efbe6359a2271b2052b05519b41
Ver0018 <rogerpantilyowger@gmail.com>
2026-01-07 04:38:07 +0800

    update json

--- lib/axios.ts
+++ lib/axios.ts
@@ -2,11 +2,12 @@ import axios from "axios"
 
 // const API_URL = process.env.EXPO_PUBLIC_API_URL || "http://localhost:4000/api"
 // const API_URL = process.env.EXPO_PUBLIC_API_URL || "http://10.0.2.2:4000/api"
-const API_URL = "https://fitness-race-api.onrender.com"
+const API_URL = "https://fitness-race-api.onrender.com/api"
 
 export function runApi(token?: string) {
     return axios.create({
         baseURL: `${API_URL}/`,
+        timeout: 15000,
         headers: {
             "Content-Type": "application/json",
             ...(token ? { Authorization: `Bearer ${token}` } : {}),
