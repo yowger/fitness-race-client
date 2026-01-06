6ea25ee1d19d10dea01e47fff6af65c447f88d4d
Ver0018 <rogerpantilyowger@gmail.com>
2026-01-06 22:54:16 +0800

    update

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 // const API_URL = process.env.EXPO_PUBLIC_API_URL || "http://localhost:4000/api"
-const API_URL = process.env.EXPO_PUBLIC_API_URL || "http://10.0.2.2:4000/api"
+// const API_URL = process.env.EXPO_PUBLIC_API_URL || "http://10.0.2.2:4000/api"
+const API_URL = "https://fitness-race-api.onrender.com"
 
 export function runApi(token?: string) {
     return axios.create({
